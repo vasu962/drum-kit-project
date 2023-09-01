690f77b2d14e3fac51d381ed42542b4f0e39b687
vasuu_kk <83589227+vasu962@users.noreply.github.com>
2023-09-01 11:37:30 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,74 @@
+
+
+//Detecting Button press
+
+var numberOfDrumButtom = document.querySelectorAll(".drum").length;
+
+
+for(var i=0; i<numberOfDrumButtom; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function (){
+        
+        var buttonInnerHTML = this.innerHTML;
+
+        makeShound(buttonInnerHTML);
+        
+    });
+
+}
+
+//Detecting Keyboard press
+
+document.addEventListener("keypress", function (event){
+
+       makeShound(event.key);
+})
+
+function makeShound(key){
+
+    switch (key) {
+        case "w":
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+
+        case "a":
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+
+        case "s":
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+
+        case "d":
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+
+        case "j":
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+
+        case "k":
+        var kickBass = new Audio("sounds/kick-bass.mp3");
+        kickBass.play();
+        break;
+
+        case "l":
+        var audio = new Audio("sounds/snare.mp3");
+        audio.play();
+        break;
+
+        default:
+    }
+}
+
+
+
+
+
+
+
